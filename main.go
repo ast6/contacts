35522ee1893f2f40e6e66906931efe4d7579ee92
Mykola <fedkoggot@gmail.com>
2018-02-09 15:49:39 +0200

    //

--- main.go
+++ main.go
@@ -4,15 +4,18 @@ import (
 	"log"
 	"net/http"
 
+	"fmt"
+
 	"github.com/ast6/contacts/Resources"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	Resources.Migration() //create table
 
-	Resources.Migration()
 	router := mux.NewRouter()
 	router.HandleFunc("/add", Resources.Add).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", router))
+	fmt.Println(router)
 }
